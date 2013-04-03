f4d0fb64a2aa42d985dbf25bb5e83ec9ae055ba5
Ron Kurr <kurr@kurron.org>
2013-04-03 11:21:52 -0400

    Use logger instead of println

--- src/test/groovy/org/kurron/profile/ProfileIntegrationTest.groovy
+++ src/test/groovy/org/kurron/profile/ProfileIntegrationTest.groovy
@@ -1,6 +1,8 @@
 package org.kurron.profile
 
 import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests
 
@@ -10,9 +12,11 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests
 @ContextConfiguration
 class ProfileIntegrationTest extends AbstractJUnit4SpringContextTests
 {
+    private final Logger logger = LoggerFactory.getLogger( ProfileIntegrationTest.class )
+
     @Test
     public void given_when_then() {
-        println 'Hello, world!'
+        logger.debug( 'Hello, world!' )
     }
 
 }
