7aa27f26572b3bcbd66e4246a1e2bb3f646dfe5b
Ron Kurr <kurr@kurron.org>
2013-04-03 14:11:16 -0400

    Added logfaces configuration files to match the Spring profiles.  The logfaces one doesn't work because I can't legally put the libraries into a public area.

--- src/test/groovy/org/kurron/profile/ProfileIntegrationTest.groovy
+++ src/test/groovy/org/kurron/profile/ProfileIntegrationTest.groovy
@@ -3,9 +3,12 @@ package org.kurron.profile
 import org.junit.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests
+
+import java.text.SimpleDateFormat
 
 /**
  * Integration test to prove out the profile switching.
@@ -20,7 +23,21 @@ class ProfileIntegrationTest extends AbstractJUnit4SpringContextTests
 
     @Test
     public void given_when_then() {
-        logger.error( theMessageGenerator.message() )
+        Random random = new Random()
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd kk:mm:ss:SSS")
+
+        MDC.put( "application-type", 'OxBABE' );
+        MDC.put( "application-instance", Integer.toHexString( random.nextInt( Integer.MAX_VALUE ) ).toUpperCase() );
+        MDC.put( "host-ip", '192.168.1.1' );
+        MDC.put( "username", 'logan' );
+
+        10.times {
+            MDC.put( "message-code", Integer.toString( random.nextInt( Integer.MAX_VALUE ) ) );
+            MDC.put( "work-unit", Integer.toHexString( random.nextInt( Integer.MAX_VALUE ) ).toUpperCase() );
+            MDC.put( "time", formatter.format( new Date( System.currentTimeMillis() ) ) );
+            MDC.put( "sequence-number", it.toString() );
+            logger.error( theMessageGenerator.message() )
+        }
     }
 
 }
